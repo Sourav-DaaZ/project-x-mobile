8739d1b13b77cb112909574ff501a13a80bc0559
q-0z <36612185+q-0z@users.noreply.github.com>
2022-07-04 03:22:12 +0530

    FCM  changes

--- src/App.js
+++ src/App.js
@@ -7,33 +7,73 @@
  */
 
 import React from 'react';
-import {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Node } from 'react';
+import
+  {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+  } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import
+  {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+  } from 'react-native/Libraries/NewAppScreen';
 
 import RootAuthenticationRoutes from './routes/index';
+import Routs from './routes';
+
+import { Provider, useStore } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider as PaperProvider } from 'react-native-paper';
+import GlobalThemeProvider from './contextProviders/globalThemeProvider';
+
+import AuthReducer from './store/reducers/auth';
+import DetailsReducer from './store/reducers/userDetails';
+
+import messaging from '@react-native-firebase/messaging';
+
+if ( __DEV__ )
+{
+  import( './reactotronConfig' ).then( () => console.log( 'Reactotron Configured' ) );
+}
+
+const rootReducer = combineReducers( {
+  auth: AuthReducer,
+  details: DetailsReducer,
+} );
+
+const store = createStore( rootReducer, applyMiddleware( thunk ) );
+
+messaging().setBackgroundMessageHandler( async remoteMessage =>
+{
+  console.log( 'Message handled in the background!', remoteMessage );
+} );
 
 
-const App = () => {
+const App = () =>
+{
   return (
-    <RootAuthenticationRoutes/>
- 
+    <RootAuthenticationRoutes />
+
   );
+  <Provider store={ store }>
+    <GlobalThemeProvider>
+      <PaperProvider>
+        <Routs />
+      </PaperProvider>
+    </GlobalThemeProvider>
+  </Provider>
+  )
 };
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,107 @@
 import * as React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Text, Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
+import { useSelector, shallowEqual } from 'react-redux';
+import SnackBar from '../sharedComponents/snackbar';
+import Loader from '../sharedComponents/loader';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useDispatch } from 'react-redux';
+import { tokenUpdate } from '../store/actions';
+import Modal from '../sharedComponents/modal';
+import OutsideAuthApi from '../services/outSideAuth';
+import { UpdateButton, UpdateTitle, UpdateDescription, UpdateWrapper, ButtonWrapper, CancelText } from './style';
+import defaultValue from '../constants/defaultValue';
+import * as FCMNotificationHandler from "../services/Google/Firebase/FCMNotificationHandler";
 
-import Loading from '../common/layout/loading'
+const AuthenticationRoutes = React.lazy( () => import( './authRouters' ).then( module => ( { default: module.AuthRouters } ) ) );
 
-const InsideAuthenticationRoutesNative =React.lazy( () => import('./InsideAuthenticationRoutes').then(module => ({ default: module.InsideAuthenticationRoutesNative})));
-const InsideAuthenticationRoutes =React.lazy( () => import('./InsideAuthenticationRoutes').then(module => ({ default: module.InsideAuthenticationRoutes})));
+function Routs ( props )
+{
+  const [ show, setShow ] = useState( false );
+  const [ updatePopup, setUpdatePopup ] = useState( null );
+  const authStore = useSelector( ( state ) => state.auth, shallowEqual );
+  const dispatch = useDispatch();
 
-function RootAuthenticationRoutes() {
-  return (
-    <React.Suspense fallback={
-      <Loading/>
+  import { NavigationContainer } from '@react-navigation/native';
+
+  import Loading from '../common/layout/loading'
+
+  const InsideAuthenticationRoutesNative = React.lazy( () => import( './InsideAuthenticationRoutes' ).then( module => ( { default: module.InsideAuthenticationRoutesNative } ) ) );
+  const InsideAuthenticationRoutes = React.lazy( () => import( './InsideAuthenticationRoutes' ).then( module => ( { default: module.InsideAuthenticationRoutes } ) ) );
+
+  function RootAuthenticationRoutes ()
+  {
+    return (
+      <React.Suspense fallback={
+        <Loading />
       }>
         <NavigationContainer>
-            <InsideAuthenticationRoutes/>
+          <InsideAuthenticationRoutes />
         </NavigationContainer>
-    </React.Suspense>
-  );
+      </React.Suspense>
+  const fetchCredentials = async () =>
+    {
+      const data = JSON.parse( await AsyncStorage.getItem( 'token' ) || "{}" );
+      dispatch( tokenUpdate( {
+        access_token: data.access_token,
+        refresh_token: data.refresh_token
+      } ) );
+      OutsideAuthApi()
+        .appConfigApi()
+        .then( ( res ) =>
+        {
+          setUpdatePopup( res.data );
+        } ).catch( ( x ) =>
+        {
+          console.log( x );
+        } )
+    }
+
+    useEffect( () =>
+    {
+      fetchCredentials();
+    }, [] )
+    useEffect( () =>
+    {
+      if ( authStore.message.msg !== '' )
+      {
+        setShow( true );
+      }
+    }, [ authStore.message ] )
+
+    useEffect( () =>
+    {
+      //FCM handle
+      FCMNotificationHandler.requestUserPermission();
+      FCMNotificationHandler.NotifinationListener();
+    } );
+    return (
+      <React.Suspense fallback={
+        <Text>Loading</Text>
+      }>
+        <SnackBar show={ show } text={ authStore.message.msg } type={ authStore.message.type } onDismiss={ () => setShow( false ) } />
+        <Loader show={ authStore.loading } />
+        <NavigationContainer>
+          <AuthenticationRoutes { ...props } islogin={ authStore.access_token && authStore.access_token !== '' } />
+        </NavigationContainer>
+        { updatePopup && defaultValue.appVersion[ Platform.OS ] < updatePopup.buildVersion[ Platform.OS ] ? <Modal show={ updatePopup && defaultValue.appVersion[ Platform.OS ] < updatePopup.buildVersion[ Platform.OS ] } onClose={ !( defaultValue.appVersion[ Platform.OS ] < updatePopup.minBuildVersion[ Platform.OS ] ) ? () => setUpdatePopup( null ) : null }>
+          <UpdateWrapper>
+            <UpdateTitle critical={ defaultValue.appVersion[ Platform.OS ] < updatePopup.minBuildVersion[ Platform.OS ] }>Update Alert!</UpdateTitle>
+            <UpdateDescription mode="contained">{ updatePopup.updateDetails[ Platform.OS ] }</UpdateDescription>
+            <ButtonWrapper>
+              { !( defaultValue.appVersion[ Platform.OS ] < updatePopup.minBuildVersion[ Platform.OS ] ) ? <UpdateButton mode="outlined" textColor='#191B49'><CancelText>cancel</CancelText></UpdateButton> : null }
+              <UpdateButton full={ defaultValue.appVersion[ Platform.OS ] < updatePopup.minBuildVersion[ Platform.OS ] } mode="contained">Update</UpdateButton>
+            </ButtonWrapper>
+          </UpdateWrapper>
+        </Modal> : null
+        }
+      </React.Suspense >
+    );
 
 
-}
+  }
 
 
 
-export default RootAuthenticationRoutes;
+  export default RootAuthenticationRoutes;
